7897814b70d9c9096a4285075e352b3cab3dfaca
Haleigh <haleighvalentine@gmail.com>
2024-05-06 15:25:29 +0200

    Remove instruction comments

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
-
 // Given Parameters
 const vel = 10000; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
